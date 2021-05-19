1d2afaa88dbaf6be4565f99fd6580b3bbee75db6
Leonardo Coelho <leon.g.coelho@gmail.com>
2021-05-19 14:49:08 -0300

    temporario

--- ServerUDP.py
+++ ServerUDP.py
@@ -1,69 +1,126 @@
-import os
+# This is server code to send video and audio frames over UDP/TCP
+
+import cv2, imutils, socket
+import numpy as np
+import time
+import base64
+import threading, wave, pyaudio,pickle,struct
 import sys
-try: 
-    import cv2, imutils, socket
-    import numpy as np
-    import time
-    import base64
-except:
-    os.system('pip install -r requirements.txt')
-    import cv2, imutils, socket
-    import numpy as np
-    import time
-    import base64
+import queue
+import os
+# For details visit pyshine.com
+q = queue.Queue(maxsize=10)
 
+filename =  'tres_espias_demais.mp4'
+command = "ffmpeg -i {} -ab 160k -ac 2 -ar 44100 -vn {}".format(filename,'temp.wav')
+os.system(command)
 
-BUFFSIZE = 65536
-serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, BUFFSIZE)
+BUFF_SIZE = 65536
+server_socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+server_socket.setsockopt(socket.SOL_SOCKET,socket.SO_RCVBUF,BUFF_SIZE)
+host_name = socket.gethostname()
+host_ip = '192.168.1.65'#  socket.gethostbyname(host_name)
+print(host_ip)
+port = 8081
+socket_address = (host_ip,port)
+server_socket.bind(socket_address)
+print('Listening at:',socket_address)
 
-hostname = socket.gethostname()
-try:
-    hostip = sys.argv[1] #'192.168.1.65' #socket.gethostbyname(hostname)
-except:
-    hostip = '192.168.1.65' #meu IP
-port = 8001
-#print("Address =>", (hostip+':'+str(port)), '\n')
+vid = cv2.VideoCapture(filename)
+FPS = vid.get(cv2.CAP_PROP_FPS)
+global TS
+TS = (0.5/FPS)
+BREAK=False
+print('FPS:',FPS,TS)
+totalNoFrames = int(vid.get(cv2.CAP_PROP_FRAME_COUNT))
+durationInSeconds = float(totalNoFrames) / float(FPS)
+d=vid.get(cv2.CAP_PROP_POS_MSEC)
+print(durationInSeconds,d)
 
-sAddress = (hostip, port)
-serverSocket.bind(sAddress)
-print('Server running at', sAddress, '...')
+def BufferCreate():
+   
+    WIDTH=400
+    while(vid.isOpened()):
+        try:
+            _,frame = vid.read()
+            frame = imutils.resize(frame,width=WIDTH)
+            q.put(frame)
+        except:
+            os._exit(1)
+    print('Player closed')
+    BREAK=True
+    vid.release()
+	
 
-fps, st, framesToCount, cnt = (0,0,20,0)
-
-
-while True:
-    msg, cAddress = serverSocket.recvfrom(BUFFSIZE)
-    print('GOT connection from', cAddress)
-    filen = msg.decode("utf-8")
-    print(filen) 
-
-    try:
-        if('.mp4' in filen):
-            filename = cv2.VideoCapture(filen)
-            WIDTH=400
-            while(filename.isOpened()):
-                _,frame = filename.read()
-                frame = imutils.resize(frame, width=WIDTH)
-                encoded, buffer = cv2.imencode('.jpg', frame, [cv2.IMWRITE_JPEG_QUALITY,80])
-                message = base64.b64encode(buffer)
-
-                serverSocket.sendto(message, cAddress)
-                frame = cv2.putText(frame, ('FPS: '+str(fps)), (10,40), cv2.FONT_HERSHEY_COMPLEX, 0.7, (0, 0, 255), 2)
-                cv2.imshow('TRANSMITTING VIDEO...',frame)
-                key = cv2.waitKey(1) & 0xFF
-                if (key == ord('q')):
-                    print("Server stopped\n")
-                    serverSocket.close()
-                    break
-                if cnt == framesToCount:
-                    try:
-                        fps = round(framesToCount / (time.time() - st))
-                        st = time.time()
-                        cnt = 0
-                    except:
+def VideoStreaming():
+    global TS
+    fps,st,frames_to_count,cnt = (0,0,1,0)
+    cv2.namedWindow('TRANSMITTING VIDEO')        
+    cv2.moveWindow('TRANSMITTING VIDEO', 10,30) 
+    while True:
+        msg,client_addr = server_socket.recvfrom(BUFF_SIZE)
+        print('GOT connection from ',client_addr)
+        WIDTH=400
+        
+        while(True):
+            frame = q.get()
+            encoded,buffer = cv2.imencode('.jpeg',frame,[cv2.IMWRITE_JPEG_QUALITY,80])
+            message = base64.b64encode(buffer)
+            server_socket.sendto(message,client_addr)
+            frame = cv2.putText(frame,'FPS: '+str(round(fps,1)),(10,40),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255),2)
+            if cnt == frames_to_count:
+                try:
+                    fps = (frames_to_count/(time.time()-st))
+                    st=time.time()
+                    cnt=0
+                    if fps>FPS:
+                        TS+=0.001
+                    elif fps<FPS:
+                        TS-=0.001
+                    else:
                         pass
-                cnt+=1
-    except:
-        msg = 'File not found!\n'
-        serverSocket.sendto(msg, sAddress)
+                except:
+                    pass
+            cnt+=1
+            
+            
+            
+            cv2.imshow('TRANSMITTING VIDEO', frame)
+            key = cv2.waitKey(int(1000*TS)) & 0xFF	
+            if key == ord('q'):
+                os._exit(1)
+                TS=False
+                break	
+                
+
+def AudioStreaming():
+    s = socket.socket()
+    s.bind((host_ip, (port-1)))
+
+    s.listen(5)
+    CHUNK = 1024
+    wf = wave.open("temp.wav", 'rb')
+    p = pyaudio.PyAudio()
+    print('server listening at',(host_ip, (port-1)))
+    stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                    channels=wf.getnchannels(),
+                    rate=wf.getframerate(),
+                    input=True,
+                    frames_per_buffer=CHUNK)
+
+    client_socket,addr = s.accept()
+
+    while True:
+        if client_socket:
+            while True:
+                data = wf.readframes(CHUNK)
+                a = pickle.dumps(data)
+                message = struct.pack("Q",len(a))+a
+                client_socket.sendall(message)
+                
+
+from concurrent.futures import ThreadPoolExecutor
+with ThreadPoolExecutor(max_workers=3) as executor:
+    executor.submit(AudioStreaming)
+    executor.submit(BufferCreate)
+    executor.submit(VideoStreaming)
